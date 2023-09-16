3b74362e9253ee5ca9d35cd81f44bc6b2db9da35
Sai Sandeep <saisandeepchandra571@gmail.com>
2023-09-16 23:38:13 +0530

    app bar added

--- lib/Screens/MessageScreen.dart
+++ lib/Screens/MessageScreen.dart
@@ -1,5 +1,6 @@
 import 'package:cemp/Screens/Components/messageCard.dart';
 import 'package:cemp/TestData/messageData.dart';
+import 'package:cemp/styles.dart';
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatefulWidget {
@@ -14,10 +15,49 @@ class _MessageScreenState extends State<MessageScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar:AppBar(
+         titleSpacing:5,
+          title:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              tx700("Sabari Momo",size:20),
+              tx400("online",size: 15)
+              ],
+            ),
+           leadingWidth:100,
+           leading:Row(
+           mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+           children: [
+            Icon(Icons.arrow_back_ios,size: 20),
+            CircleAvatar(
+              radius: 22,  
+                     
+            )
+          ],
+        ),
+      ),
+
+
+
         body: Column(
           children: [
             // type your code here
-            Text("PRASAD"),
+
+
+
+
+
+
+          
+    
+    
+
+
+
+
+
+
+
 
             //---------------------------- code to generate message list
             for (var data in mlist)
