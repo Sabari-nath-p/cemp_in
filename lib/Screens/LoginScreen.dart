1d876d582b67f0387c63cda75c64c677f9bf6364
unknown <nandanasanal321@gmail.com>
2023-09-17 21:21:05 +0530

    login file

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -15,21 +15,70 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Scaffold(
       body: Column(
         children: [
-          Container(width: MediaQuery.of(context).size.width,
-          height: 150,
-          alignment: Alignment.bottomCenter,
-          color: Colors.amber,
+          Container(
+            width:MediaQuery.of(context).size.width,
+            height: 300,
+            color: Colors.white,
           ),
+          Text("Login for account",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700,color: Colors.black),),
+           Text("kindly make account to meet new friends and date in your favourite location",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.grey),textAlign: TextAlign.center,),
+          SizedBox(height: 5,),
+            Align(
+              child:Padding(padding: EdgeInsets.only(left: 3),
+               child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: ("PRP Code"),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(5),)
+                      ),
+              )
+             
+              ),
+            ),
+            SizedBox(height: 3,),
+             Align(
+              child:Padding(padding: EdgeInsets.only(left: 3),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: ("Password"),
+                        border: OutlineInputBorder()
+                      ),
+                         ),
+              )
+               
+             ),
+            Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(padding: EdgeInsets.only(right: 1.0),
+                        child: Text("Forgot password?",style: TextStyle(fontSize: 13,color: Colors.grey),),)
+                      ),
+                      SizedBox(height: 5,),
+                      Expanded(child: Container()),
           InkWell(
               onTap: () {
                 Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) => HomeMain()));
               },
               child: Container(
-                width: 100,
-                color: Colors.amber,
-                height: 100,
-              ))
+                width: MediaQuery.of(context).size.width,
+                height:50,
+                padding: EdgeInsets.only(left: 3),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.pink,),
+                
+                alignment: Alignment.center,
+                child: Text("Sign in",style:TextStyle(fontSize: 17,color: Colors.white),),
+                
+              )),
+              SizedBox(height: 5,),
+               Container( 
+                        alignment: Alignment.center,
+                        child:
+                            Text("By signing up you agree to our Terms and Conditions and Privacy Policy",style: TextStyle(fontSize: 13,color:Colors.black),),
+                          ),
+                           SizedBox(height: 5,),
+                          
+                     
 // type login page code here
         ],
       ),
